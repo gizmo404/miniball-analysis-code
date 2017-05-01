75a87a004e57cc23c032054c8ff1ce45de1442bf
Liam Gaffney <lpgaff@gmail.com>
2017-05-01 23:18:24 +0200

    Cleaning up whitespace, etc.

--- MedToRoot/Datas.cc
+++ MedToRoot/Datas.cc
@@ -5,42 +5,48 @@ ClassImp(DgfData)
 ClassImp(AdcSubEvent)
 
 // AdcData
-AdcData::AdcData()
-{
-  //cout<<__PRETTY_FUNCTION__<<endl;
+AdcData::AdcData() {
+	
+	//cout<<__PRETTY_FUNCTION__<<endl;
+	
 }
 
 AdcData::AdcData(unsigned short moduleNumber, long long time, AdcSubEvent* adcSubEvent, bool LaserOn, bool FieldUp, bool FieldDown)
-  :fModuleNumber(moduleNumber), fTime(time), fLaserOn(LaserOn), fFieldUp(FieldUp), fFieldDown(FieldDown)
-{
-  //  cout<<__PRETTY_FUNCTION__<<endl;
-  fAdcSubEvent = *adcSubEvent;
+:fModuleNumber(moduleNumber), fTime(time), fLaserOn(LaserOn), fFieldUp(FieldUp), fFieldDown(FieldDown) {
+	
+	//  cout<<__PRETTY_FUNCTION__<<endl;
+	fAdcSubEvent = *adcSubEvent;
+	
 }
 
-AdcData::~AdcData()
-{
-  //  cout<<__PRETTY_FUNCTION__<<endl;
-  //fAdcSubEvent->Clear();
+AdcData::~AdcData() {
+	
+	//  cout<<__PRETTY_FUNCTION__<<endl;
+	//fAdcSubEvent->Clear();
+	
 }
 
 // DgfData
-DgfData::DgfData()
-{
-  //cout<<__PRETTY_FUNCTION__<<endl;
+DgfData::DgfData() {
+	
+	//cout<<__PRETTY_FUNCTION__<<endl;
+	
 }
 
 DgfData::DgfData(unsigned short moduleNumber, unsigned short channel, DgfSubEvent* dgfSubEvent, unsigned short multiplicity)
-  :fModuleNumber(moduleNumber), fChannel(channel), fMultiplicity(multiplicity)
-{
-  //cout<<__PRETTY_FUNCTION__<<endl;
-  fEventTime = dgfSubEvent->GetEventTime();
-  fEnergy = dgfSubEvent->GetEnergy(channel);
-  fFastTriggerTime = dgfSubEvent->GetFastTriggerTime(channel);
-  fLongFastTriggerTime = dgfSubEvent->GetLongFastTriggerTime(channel);
-  dgfSubEvent->GetUserValues(channel, fUserValues);
+:fModuleNumber(moduleNumber), fChannel(channel), fMultiplicity(multiplicity) {
+	
+	//cout<<__PRETTY_FUNCTION__<<endl;
+	fEventTime = dgfSubEvent->GetEventTime();
+	fEnergy = dgfSubEvent->GetEnergy(channel);
+	fFastTriggerTime = dgfSubEvent->GetFastTriggerTime(channel);
+	fLongFastTriggerTime = dgfSubEvent->GetLongFastTriggerTime(channel);
+	dgfSubEvent->GetUserValues(channel, fUserValues);
+	
 }
 
-DgfData::~DgfData()
-{
-  //cout<<__PRETTY_FUNCTION__<<endl;
+DgfData::~DgfData() {
+	
+	//cout<<__PRETTY_FUNCTION__<<endl;
+	
 }
--- MedToRoot/Datas.hh
+++ MedToRoot/Datas.hh
@@ -7,105 +7,61 @@
 
 #include "SubEvents.hh"
 
-class AdcData : public TObject
-{
+class AdcData : public TObject {
+	
 public:
-  AdcData();
-  AdcData(unsigned short, long long, AdcSubEvent*, bool, bool, bool);
-  ~AdcData();
-
-  inline long long Time()
-  {
-    return fTime;
-  }
-
-  inline unsigned short ModuleNumber()
-  {
-    return fModuleNumber;
-  }
-
-  inline bool LaserOn()
-  {
-    return fLaserOn;
-  };
-  
-  inline bool FieldUp()
-  {
-    return fFieldUp;
-  };
-  
-  inline bool FieldDown()
-  {
-    return fFieldDown;
-  };
-
-  inline AdcSubEvent* SubEvent()
-  {
-    return &fAdcSubEvent;
-  };
-
+	AdcData();
+	AdcData(unsigned short, long long, AdcSubEvent*, bool, bool, bool);
+	~AdcData();
+	
+	inline long long Time() { return fTime; }
+	inline unsigned short ModuleNumber() { return fModuleNumber; }
+	inline bool LaserOn() { return fLaserOn; }
+	inline bool FieldUp() { return fFieldUp; }
+	inline bool FieldDown() { return fFieldDown; }
+	inline AdcSubEvent* SubEvent() { return &fAdcSubEvent; }
+	
 protected:
-  unsigned short fModuleNumber;
-  unsigned long long fTime;
-
-  bool fLaserOn;
-  bool fFieldUp;
-  bool fFieldDown;
-
-  AdcSubEvent fAdcSubEvent;
-
-  ClassDef(AdcData, 3)
+	unsigned short fModuleNumber;
+	unsigned long long fTime;
+	
+	bool fLaserOn;
+	bool fFieldUp;
+	bool fFieldDown;
+	
+	AdcSubEvent fAdcSubEvent;
+	
+	ClassDef(AdcData, 3);
+	
 };
 
-class DgfData : public TObject
-{
+class DgfData : public TObject {
+	
 public:
-  DgfData();
-  DgfData(unsigned short, unsigned short, DgfSubEvent*, unsigned short = 1);
-  ~DgfData();
-
-  inline unsigned short ModuleNumber()
-  {
-    return fModuleNumber;
-  };
-
-  inline unsigned short Channel()
-  {
-    return fChannel;
-  };
-
-  inline unsigned short Multiplicity()
-  {
-    return fMultiplicity;
-  };
-
-  inline unsigned short Energy()
-  {
-    return fEnergy;
-  };
-
-  inline unsigned short* UserValues()
-  {
-    return fUserValues;
-  };
-
-  inline long long Time()
-  {
-    return fLongFastTriggerTime;
-  };
-
+	DgfData();
+	DgfData(unsigned short, unsigned short, DgfSubEvent*, unsigned short = 1);
+	~DgfData();
+	
+	inline unsigned short ModuleNumber() { return fModuleNumber; }
+	inline unsigned short Channel() { return fChannel; }
+	inline unsigned short Multiplicity() { return fMultiplicity; }
+	inline unsigned short Energy() { return fEnergy; }
+	inline unsigned short* UserValues() { return fUserValues; }
+	inline long long Time() { return fLongFastTriggerTime; }
+	
 protected:
-  unsigned short fModuleNumber;
-  unsigned short fChannel;
-  unsigned short fMultiplicity;
-
-  unsigned long long fEventTime;
-  unsigned short fEnergy;
-  unsigned short fFastTriggerTime;
-  unsigned long long fLongFastTriggerTime;
-  unsigned short fUserValues[NumberOfUserValues];
-
-  ClassDef(DgfData, 2)
+	unsigned short fModuleNumber;
+	unsigned short fChannel;
+	unsigned short fMultiplicity;
+	
+	unsigned long long fEventTime;
+	unsigned short fEnergy;
+	unsigned short fFastTriggerTime;
+	unsigned long long fLongFastTriggerTime;
+	unsigned short fUserValues[NumberOfUserValues];
+	
+	ClassDef(DgfData, 2);
+	
 };
 
 #endif
